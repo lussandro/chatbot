5461b73a56bb8e1a240708beea1f2a6ea0c64cad
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-01-29 17:21:13 -0300

    alteracoes

--- treino.py
+++ treino.py
@@ -2,7 +2,6 @@ import spacy
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from spacy.cli import download
-import yaml
 
 download("en_core_web_sm")
 
@@ -14,10 +13,9 @@ chatbot = ChatBot(
     "MeuChatBot",
     tagger_language=ENGSM
 )
-# Resto do seu código...
+
 
 trainer = ChatterBotCorpusTrainer(chatbot)
 
-# Aqui, você precisará especificar o caminho para os arquivos baixados
-# Por exemplo: 'Caminho/para/o/arquivo/portuguese/conversations.yml'
+
 trainer.train("/root/chatbot/")
