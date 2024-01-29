e58c778c462b122fc11c978616cc2c7eee2f8296
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-01-29 15:40:04 -0300

    alteracoes

--- bot.py
+++ bot.py
@@ -16,4 +16,4 @@ def webhook():
 
 if __name__ == '__main__':
     # Inicia o servidor na porta 5000
-    app.run(host='.0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
