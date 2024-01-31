8ec8542ec810f6a5b363988ef5e592e55785a4d3
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-01-31 17:02:00 -0300

    Testes

--- bot.py
+++ bot.py
@@ -38,4 +38,4 @@ def webhook():
     return jsonify({"message": "Dados recebidos e processados com sucesso!"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0.0',debug=True, port=5000)
