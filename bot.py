3587888d705fc45e80945530d89504cfb3cdbd77
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-01-31 16:58:55 -0300

    Testes

--- bot.py
+++ bot.py
@@ -1,26 +1,41 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contatos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.String(80), unique=True, nullable=False)
+    nome = db.Column(db.String(120), nullable=False)
+    instancia = db.Column(db.String(120), nullable=False)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Recebendo os dados do webhook
     data = request.json
-    
-    # Extraindo os valores específicos
-    remote_jid = data['data']['key']['remoteJid']
+    print("Dados recebidos:", data)
+
+    remote_jid = data['data']['key']['remoteJid'].replace('@s.whatsapp.net', '')
     message = data['data']['message']['conversation']
+    push_name = data['data']['pushName']
     sender = data['sender']
 
-    # Imprimindo os valores
-    print("remoteJid:", remote_jid)
-    print("message:", message)
-    print("sender:", sender)
+    # Verificando se o contato já existe
+    contato_existente = Contato.query.filter_by(numero=remote_jid).first()
+    if not contato_existente:
+        novo_contato = Contato(numero=remote_jid, nome=push_name, instancia=sender)
+        db.session.add(novo_contato)
+        db.session.commit()
+        print("Contato adicionado:", novo_contato.numero, novo_contato.nome, novo_contato.instancia)
+    else:
+        print("Contato já existe:", contato_existente.numero, contato_existente.nome, contato_existente.instancia)
 
-    # Armazenando os valores (aqui você pode adicionar seu código de armazenamento)
-
-    # Resposta de sucesso
     return jsonify({"message": "Dados recebidos e processados com sucesso!"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=5000)
+    app.run(debug=True, port=5000)
