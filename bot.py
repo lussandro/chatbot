2cf6dce45f0e6f70fa6bfbcc45c8976196fc1dd4
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-01-31 20:13:26 -0300

    Testes

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import requests
